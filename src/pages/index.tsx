7c870bf94c7d43c99623c972d31d3ad549a7b197
DiazNugraha <diaznugraha00@gmail.com>
2024-07-18 00:55:43 +0700

    init frontend

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`relative flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <div className=" dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"></div>
+      </div>
+
+      <div className="bg-slate-200/10 border-[1px] border-white/10 h-[80vh] w-[60vw]"></div>
+
+      <div className="top-0 -left-4 flex place-items-center after:absolute after:h-[200px] after:w-full sm:after:w-[580px] after:-translate-x-1/2 after:rounded-full after:bg-gradient-radial after:from-transparent after:to-white after:blur-2xl after:content-[''] before:-z-20 before:h-[120px] before:w-full sm:before:w-[220px] before:-translate-x-1/3 before:bg-gradient-conic before:from-blue-200 before:via-sky-200 before:blur-2xl before:content-[''] after:dark:bg-gradient-to-br after:dark:from-transparent before:dark:to-blue-700/10 before:dark:from-sky-900 before:dark:via-[#0141ff]/40 after:lg:h-[360px]">
+        <div className=" dark:drop-shadow-[0_0_0.3rem_#ffffff50] dark:invert"></div>
+      </div>
+    </main>
+  );
+}
